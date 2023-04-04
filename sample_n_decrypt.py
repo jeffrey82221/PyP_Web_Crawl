963e55f351928a4a4bd82ca1a18943023c7be3cf
jeffrey82221 <jeffrey82221@gmail.com>
2023-04-05 01:25:30 +0800

    fix: arg

--- sample_n_decrypt.py
+++ sample_n_decrypt.py
@@ -9,16 +9,16 @@ import tqdm
 from src.json_tool import json_tool
 
 
-def run(src_path, sample_size):
+def run(src_path, target_path, sample_size):
     files = os.listdir(src_path)
     sampled_files = random.sample(files, sample_size)
-    if not os.path.exists("decrypted"):
-        os.mkdir("decrypted")
+    if not os.path.exists(target_path):
+        os.mkdir(target_path)
     for file in tqdm.tqdm(sampled_files):
         json_obj = json_tool.load(f"{src_path}/{file}")
-        with open(f"decrypted/{file}", "w", encoding="utf-8") as f:
+        with open(f"{target_path}/{file}", "w", encoding="utf-8") as f:
             json.dump(json_obj, f, ensure_ascii=False, indent=2)
 
 
 if __name__ == "__main__":
-    run(sys.argv[1], int(sys.argv[2]))
+    run(sys.argv[1], sys.argv[2], int(sys.argv[3]))
