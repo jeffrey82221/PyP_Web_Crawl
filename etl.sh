540767b14e2781f4d80764a6ce30f373e70d7959
jeffrey82221 <jeffrey82221@gmail.com>
2023-04-05 21:20:22 +0800

    debug: run etl on image flow

---
+++ etl.sh
@@ -0,0 +1,10 @@
+echo 'Copy Entire /app to outside'
+cp -r /app /io
+echo 'Check the arg value'
+echo $1 >> filekey.key
+echo 'RUN etl'
+python update_package_names.py
+echo 'Remove /io/app/data'
+rm -rf /io/app/data
+echo 'Copy /data Out'
+cp -r /data /io/app
